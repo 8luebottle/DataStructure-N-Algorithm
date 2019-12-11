ee0fa1b1b9da173bd969c88fd961ac3e336bb6c8
8luebottle <itbiz.irs@gmail.com>
2019-12-11 20:11:51 +0900

    HASH TABLE

---
+++ Data_Structure/hash_table.py
@@ -0,0 +1,40 @@
+from linkedListFIFO import LinkedListFIFO
+
+class HAshTableLL(object):
+    def __init__(self, size):
+        self.size  = size
+        self.slots = []
+        self._createHashTable()
+
+    def _createHashTable(self):
+        for i in range(self.size):
+            self.slots.append(LinkedListFIFO())
+
+    def _find(self, item):
+        return item % self.size
+
+    def _add(self, item):
+        return item % self.size
+
+    def _delete(self, item):
+        index = self._find(item)
+        self.slots[index].deleteNodeByValue(item)
+
+    def _print(self):
+        for i in range(self.size):
+            print('Slot {0}'.format(i))
+            self.slots[i]._printList()
+
+    def test_hash_tables():
+        H1 = HashTableLL(3)
+        for i in range(0, 20):
+            H1._add(i)
+        H1._print()
+        print('\n Delete 0, 1, 2')
+        H1._delete(0)
+        H1._delete(1)
+        H1._delete(2)
+        H1._print()
+
+    if __name__ == '__main__':
+        test_hash_tables()
