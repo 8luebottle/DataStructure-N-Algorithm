e25b81da6c8318e9e2e7d69d5a5bc5237548099b
8luebottle <itbiz.irs@gmail.com>
2019-12-16 21:55:22 +0900

    TREE

---
+++ Data_Structure/tree.py
@@ -0,0 +1,11 @@
+class SimpleTree(object):
+    def __init__(self, value=None, children=None):
+        self.value = value
+        self.children = children
+        if self.children is None:
+            self.children = []
+    def __repr__(self, level=0):
+        ret = "\t"*level + repr(self.value) + "\n"
+        for child in self.children:
+            ret += child.__repr__(level+1)
+        return ret
