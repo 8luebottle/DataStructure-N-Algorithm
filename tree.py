5e3670191ed9f6981a90fcc52ff6a8bd7a01dad5
8luebottle <itbiz.irs@gmail.com>
2019-12-19 19:57:04 +0900

    TREE : Binary Tree

---
+++ tree.py
@@ -0,0 +1,44 @@
+#BT Lists
+def BinaryTreeList(r):
+    return [r, [], []]
+
+def insertLeft(root, newBranch):
+    t = root.pop(1)
+    if len(t) > 1:
+        root.insert(1, [newBranch], t, []])
+    else:
+        root.inesrt(1, [newBranch], t []])
+    return root
+
+def insertRight(root, newBranch):
+    t = root.pop(2)
+    if len(t) > 1:
+        root.insert(2, [newBranch, [], t])
+    else:
+        root.insert(2, [newBranch, [], []])
+    return root
+
+def getRootVal(root):
+    return root[0]
+
+def setRootVal(root):
+    root[0] = newVal
+
+def getLeftChild(root):
+    return root[1]
+
+def getRightChild(root):
+    return root[2]
+
+def main():
+    r = BinaryTreeList(3)
+    insertLeft(r, 4)
+    insertLeft(r, 5)
+    insertRight(r, 6)
+    insertRight(r, 7)
+    print(getRootVal(r))
+    print(getLeftChild(r))
+    print(getRightChild(r))
+
+if __name__ == "__main__":
+    main()
