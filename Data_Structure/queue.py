42c2e906f5ede5dfa1cbd69fcdeeb8dc3f50afd5
8luebottle <itbiz.irs@gmail.com>
2019-12-21 11:44:56 +0900

    QUEUE : Queue from two stacks

--- Data_Structure/queue.py
+++ Data_Structure/queue.py
@@ -1,5 +1,49 @@
 import heapq
 
+
+class Queue(object):
+    def __init__(self):
+        self.in_stack = []
+        self.out_stack = []
+
+    def _transfer(self):
+        while self.in_stack:
+            self.out_stack.append(self.in_stack.pop())
+
+    def enqueue(self):
+        return self.in_stack.append(item)
+
+    def dequeue(self):
+        if not self.out_stack:
+            self._transfer()
+        if self.out_stack:
+            return self.out_stack.pop()
+        else:
+            print("QUEUE is Empty!")
+
+    def size(self):
+        return len(self.in_stack) + len(self.out_stack)
+
+    def peek(self):
+        if not self.out_stack:
+            self._transfer()
+        if self.out_stack:
+            return self.out_stack[-1]
+        else:
+            print("QUEUE is Empty!")
+
+    def __repr__(self):
+        if not self.out_stack:
+            self._transfer()
+        if self.out_stack:
+            return repr(self.out_stack)
+        else:
+            print("QUEUE is Empty!")
+
+    def isEmpty(self):
+        return not (bool(self.in_stack) or bool(self.out_stack))
+
+
 class PriorityQueue(object):
     def __init__(self):
         self._queue = []
