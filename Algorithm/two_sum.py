793b1c85a3dfa158f4e552030c3416f424cb84d5
8luebottle <itbiz.irs@gmail.com>
2019-12-22 09:45:49 +0900

    ALGORITHM :Two Sum Version 2

--- Algorithm/two_sum.py
+++ Algorithm/two_sum.py
@@ -1,8 +1,21 @@
-def two_sum(nums, target)
+# V.01 | Brute Force
+def two_sum(nums, target):
     answer = list()
     for idx, ele in enumerate(nums):
         for i in range(idx+1, len(nums)):
             if (ele + nums[i]) == target:
                 answer.append(idx)
                 answer.append(i)
+    return answer
+
+
+# V.02 | HashMap
+def two_sum(nums, target):
+    compliments = {}
+    answer = []
+    for idx, ele in enumerate(nums):
+        if compliments.get(ele) is None:
+            compliments[target-ele] = idx
+        else:
+            answer = [compliments[ele], idx]
     return answer
