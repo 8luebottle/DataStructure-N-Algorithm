9bed9ed5ea2190aaf13501d4c0f2fa79c49a385f
8luebottle <itbiz.irs@gmail.com>
2020-01-01 22:03:41 +0900

    013: Merge Two Sorted Lists (Python)

---
+++ Algorithm/merge_two_sorted_lists.py
@@ -0,0 +1,22 @@
+# singly linked list
+
+class ListNode:
+    def __init__(self, x):
+        self.val  = x
+        self.next = None
+
+class Solution:
+    def merge_two_lists(self, l1, l2):
+        curr = dummy = ListNode(0)
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1       # Pointing to l1
+                l1        = l1.next
+            else:
+                curr.next = l2
+                l2        = l2.next
+            curr = curr.next
+        curr.next = l1 or l2         # Exceptional Case
+
+        return dummy.next
