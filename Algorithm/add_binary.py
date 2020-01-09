f451ec6bdb9fe762e4621e3d1fa679259e38e991
8luebottle <itbiz.irs@gmail.com>
2020-01-09 17:50:48 +0900

    021: Add Binary (Python)

---
+++ Algorithm/add_binary.py
@@ -0,0 +1,9 @@
+def addBinary(a, b):
+    a = int(a, 2)
+    b = int(b, 2)
+    return (bin(a + b)[2:])
+
+"""
+Runtime: 28 ms, faster than 83.19% of Python3 online submissions for Add Binary.
+Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Add Binary
+"""
