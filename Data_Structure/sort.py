253ab5e9c630305d29584dcb418847db2b903818
8luebottle <itbiz.irs@gmail.com>
2020-01-18 22:02:03 +0900

    SORT : Insertion Sort

--- Data_Structure/sort.py
+++ Data_Structure/sort.py
@@ -1,8 +1,31 @@
 """
 [Data Structure] : Sort
+insertion Sort
 Bubble Sort
 Gnome Sort
 """
+
+# Insertion Sort
+def insertion_sort(seq):
+    for i in range(1, len(seq)):
+        j = i
+        while j > 0 and seq[j-1] > seq[j]:
+            seq[j-1], seq[j] = seq[j], seq[j-1]
+            j -= 1
+    return seq
+
+def insertion_sort_rec(seq, i=None):
+    if i is None:
+        i = len(seq) - 1
+    if i == 0:
+        return i
+    insertion_sort_req(seq, i-1)
+    j = i
+    while j > 0 and seq[j-i] > seq[j]:
+        seq[j-1], seq[j] = seq[j], seq[j-1]
+        j -= 1
+    return seq
+
 
 # Bubble Sort
 def bubble_sort(seq):
