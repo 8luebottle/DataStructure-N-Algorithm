473f290ee1eb6c7cffa907bc20a5380b70c13956
8luebottle <itbiz.irs@gmail.com>
2020-01-19 00:15:27 +0900

    SORT : Selection Sort

--- Data_Structure/sort.py
+++ Data_Structure/sort.py
@@ -1,9 +1,22 @@
 """
 [Data Structure] : Sort
-insertion Sort
+Selection Sort
+Insertion Sort
 Bubble Sort
 Gnome Sort
 """
+
+# Selection Sort
+def selection_sort(seq):
+    length = len(seq)
+    for i in range(length-1):
+        min_j = i
+        for j in range(i+1, length):
+            if seq[min_j] > seq[j]:
+                min_j = j
+        seq[i], seq[min_j] = seq[min_j], seq[i]
+    return seq
+
 
 # Insertion Sort
 def insertion_sort(seq):
