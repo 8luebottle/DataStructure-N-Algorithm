2ae4beaed14cc856362577b2b051e15793b6f46c
8luebottle <itbiz.irs@gmail.com>
2020-01-31 15:53:39 +0900

    Recursion : Fibonacci (Go)

--- Data_Structure/recursion.go
+++ Data_Structure/recursion.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// Factorial
 func fac(n int) int {
 	if n <= 0 {
 		return 1
@@ -9,6 +10,25 @@ func fac(n int) int {
 	return n * fac(n-1)
 }
 
+// Fibonacci
+func fib() func() int {
+	f1 := 0
+	f2 := 1
+	return func() int {
+		f2, f1 = (f1 + f2), f2
+		return f1
+	}
+}
+
 func main() {
+	// Test Factorial
 	fmt.Println(fac(5)) // Try 5
+
+	fmt.Print("\n")
+
+	// Test Fibonacci
+	gen := fib()
+	for i := 0; i < 10; i++ {
+		fmt.Println(gen())
+	}
 }
