94d139af54e802520c47f0980015dae89f91ad4b
8luebottle <itbiz.irs@gmail.com>
2020-02-06 00:30:20 +0900

    SORT : Bubble Sort (Go)
    Bubble Sort with generateSlice

---
+++ Data_Structure/sort.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func generateSlice(size int) []int {
+	slice := make([]int, size, size)
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < size; i++ {
+		slice[i] = rand.Intn(999) - rand.Intn(999)
+	}
+	return slice
+}
+
+// Bubble Sort
+func bubbleSort(items []int) {
+	var (
+		n      = len(items)
+		sorted = false
+	)
+	for !sorted {
+		swapped := false
+		for i := 0; i < n-1; i++ {
+			if items[i] > items[i+1] {
+				items[i+1], items[i] = items[i], items[i+1]
+				swapped = true
+			}
+		}
+		if !swapped {
+			sorted = true
+		}
+		n = n - 1
+	}
+}
+
+func main() {
+	// Testing Bubble Sort
+	slice := generateSlice(30)
+	fmt.Println("Before Bubble Sort : \n", slice, "\n")
+	bubbleSort(slice)
+	fmt.Println("After Bubble Sort : \n", slice, "\n")
+}
