611dd0c934170967596a61181070b54a3e354f56
8luebottle <itbiz.irs@gmail.com>
2020-02-12 00:35:04 +0900

    QUEUE : Array based queue (JavaScript)

---
+++ Data_Structure/queue.js
@@ -0,0 +1,26 @@
+// Queue using Array
+class Queue{
+  constructor(){
+    this.queue = []
+  }
+
+  enqueue(element){
+    this.queue.push(element)
+  }
+
+  dequeue(){
+    if (this.isEmpty())
+      return 'Queue is empty'
+    return this.queue.shift()
+  }
+
+  peek() {
+    if(this.isEmpty())
+      return 'Queue is empty'
+    return this.queue[0]
+  }
+
+  isEmpty(){
+    return !this.queue.length
+  }
+}
